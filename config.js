e20ef2a4a683113674eb0679dca898ca0ff382fb
sahle-1996 <sahlesiumgoitom@gmai.com>
2025-02-17 15:29:11 +0300

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const PORT = 7777;
 
-export const mongoDBURL = 
+export const MONGO_DB_URL = 
 'mongodb+srv://sahlesiumgoitom:l4nAikGwns1ZHuLo@books-mern-fullstack.b7wch.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Mern-Fullstack'
